bbac5bfb36264e94d02f0c877c7ca794b9f023ff
Bhavik Kapadiya <bhavikkapadiya53@gmail.com>
2020-02-22 13:23:44 +0530

    a file changes

--- a.php
+++ a.php
@@ -1,19 +1,3 @@
 <?php
-	function armsrtong($n){
-		$sum = 0;
-		$rem = $n%10;
-		$sum += $rem*$rem*$rem;
-		$rem = $n/10;
-		echo $sum;  
-		if($n > 0){
-			 armsrtong($n);		
-		}else{
-
-		}
-	}
-
 	echo "branch1";
-	echo "master changes"; 
-	armsrtong(153);
-	echo "sdf"; exit;
 ?>
